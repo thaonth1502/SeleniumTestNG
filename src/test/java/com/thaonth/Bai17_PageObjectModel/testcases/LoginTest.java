91f8391a8481f280203a02ea7ec1088e4a6a37dd
thaonth1502 <huongthao89@gmail.com>
2024-08-12 17:38:57 +0700

    update

---
+++ src/test/java/com/thaonth/Bai17_PageObjectModel/testcases/LoginTest.java
@@ -0,0 +1,51 @@
+package com.thaonth.Bai17_PageObjectModel.testcases;
+
+import com.thaonth.Bai17_PageObjectModel.pages.LoginPage;
+import com.thaonth.common.BaseTest;
+import com.thaonth.constants.ConfigData;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    LoginPage loginPage;
+
+    @Test
+    public void testLoginSuccess(){
+        loginPage = new LoginPage(driver);
+
+        loginPage.loginCRM(ConfigData.EMAIL, ConfigData.PASSWORD);
+        loginPage.verifyLoginSuccess();
+    }
+
+    @Test
+    public void testLoginFailWithEmailInvalid(){
+        loginPage = new LoginPage(driver);
+
+        loginPage.loginCRM("admin123@example.com", ConfigData.PASSWORD);
+        loginPage.verifyLoginFail("Invalid email or password");
+    }
+
+    @Test
+    public void testLoginFailWithPasswordInvalid(){
+        loginPage = new LoginPage(driver);
+
+        loginPage.loginCRM(ConfigData.EMAIL, "12345678");
+        loginPage.verifyLoginFail("Invalid email or password");
+    }
+
+    @Test
+    public void testLoginFailWithEmailNull(){
+        loginPage = new LoginPage(driver);
+
+        loginPage.loginCRM("", "123456");
+        loginPage.verifyLoginFail("The Email Address field is required.");
+    }
+
+    @Test
+    public void testLoginFailWithPasswordNull(){
+        loginPage = new LoginPage(driver);
+
+        loginPage.loginCRM("admin@example.com", "");
+        loginPage.verifyLoginFail("The Password field is required.");
+    }
+}
