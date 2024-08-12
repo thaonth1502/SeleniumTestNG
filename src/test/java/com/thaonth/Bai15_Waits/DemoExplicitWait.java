6f86b4ae465de4d1075883fb72189f2208a06f69
thaonth1502 <huongthao89@gmail.com>
2024-08-12 08:20:59 +0700

    Update

--- src/test/java/com/thaonth/Bai15_Waits/DemoExplicitWait.java
+++ src/test/java/com/thaonth/Bai15_Waits/DemoExplicitWait.java
@@ -3,11 +3,13 @@ package com.thaonth.Bai15_Waits;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class DemoImplicitWait {
+public class DemoExplicitWait {
 
     WebDriver driver;
 
@@ -17,18 +19,22 @@ public class DemoImplicitWait {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        //Set timeout for implicitlyWait
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.get("https://hrm.anhtester.com/");
         driver.findElement(By.id("iusername")).sendKeys("admin_example");
         driver.findElement(By.id("ipassword")).sendKeys("123456");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
         //click on menu du an
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='Projects']")));
         driver.findElement(By.xpath("//span[normalize-space()='Projects']")).click();
+
+//        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()=Add New']")));
+        driver.findElement(By.xpath("//a[normalize-space()='Add New']")).click();
+
 //        Thread.sleep(2000);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0)); // Reset implicitWait
+
         driver.quit();
     }
 }
